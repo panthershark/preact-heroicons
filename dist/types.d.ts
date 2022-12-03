82f43d3afddd5d492196c490333413812727a9ff
panthershark <tommy@panthershark.dev>
2022-12-02 20:49:58 -0600

    check in built deps so I can hack a link to github until PR is merged

---
+++ dist/types.d.ts
@@ -0,0 +1,6 @@
+import { FunctionalComponent, JSX, Ref } from 'preact';
+import { JSXInternal } from 'preact/src/jsx';
+export type HeroIconAttrs = JSXInternal.SVGAttributes & JSXInternal.HTMLAttributes;
+export type HeroIcon = FunctionalComponent<Omit<JSX.SVGAttributes<SVGElement> & JSX.HTMLAttributes<EventTarget>, 'ref'> & {
+    ref?: Ref<SVGSVGElement>;
+}>;
