4e7b5f3e7bdab4108df762cde8bb1350d9a19d0e
panthershark <tommy@panthershark.dev>
2022-12-06 17:48:25 -0600

    wip

--- types.ts
+++ types.ts
@@ -1,10 +1,14 @@
-import { FunctionalComponent, JSX, Ref } from 'preact';
-import { JSXInternal } from 'preact/src/jsx';
+import { FunctionalComponent, JSX, Ref } from "preact";
+import { JSXInternal } from "preact/src/jsx";
 
-export type HeroIconAttrs = JSXInternal.SVGAttributes & JSXInternal.HTMLAttributes;
+export type HeroIconAttrs = JSXInternal.SVGAttributes &
+  JSXInternal.HTMLAttributes;
 
 export type HeroIcon = FunctionalComponent<
-  Omit<JSX.SVGAttributes<SVGElement> & JSX.HTMLAttributes<EventTarget>, 'ref'> & {
+  Omit<
+    JSX.SVGAttributes<SVGElement> & JSX.HTMLAttributes<EventTarget>,
+    "ref"
+  > & {
     ref?: Ref<SVGSVGElement>;
   }
 >;
